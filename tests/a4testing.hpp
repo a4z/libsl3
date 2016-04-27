20f8273c2e1d2288cf47c692fdd03fdc3267cbd8
a4z <harald.achitz@gmail.com>
2016-04-27 09:00:04 +0200

    update doc header

--- tests/a4testing.hpp
+++ tests/a4testing.hpp
@@ -1,5 +1,5 @@
 /*
---------------Copyright (c) 2009-2015 H a r a l d  A c h i t z---------------
+--------------Copyright (c) 2009-2016 H a r a l d  A c h i t z---------------
 -----------< h a r a l d dot a c h i t z at g m a i l dot c o m >------------
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
