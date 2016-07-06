317f22d0e96901ae2f9fbff3b73c7fae8f0a62ec
a4z <harald.achitz@gmail.com>
2016-07-06 15:18:02 +0200

    apply clang format

--- include/sl3/dbvalue.hpp
+++ include/sl3/dbvalue.hpp
@@ -9,17 +9,14 @@
 #ifndef SL3_DBVALUE_HPP_
 #define SL3_DBVALUE_HPP_
 
-
 #include <string>
 
 #include <sl3/config.hpp>
 #include <sl3/error.hpp>
 #include <sl3/types.hpp>
 
-
-namespace sl3 {
-
-
+namespace sl3
+{
   /**
    *  \brief
    *
@@ -39,9 +36,7 @@ namespace sl3 {
    */
   class LIBSL3_API DbValue
   {
-
   public:
-
     /**
      * \brief Constructor
      *
@@ -50,8 +45,7 @@ namespace sl3 {
      *  \param type wanted storage type
      *  If Type::Null is given, the type will be a variant.
      */
-    DbValue(Type type) noexcept;
-
+    DbValue (Type type) noexcept;
 
     /** \brief Constructor
      *
@@ -63,46 +57,42 @@ namespace sl3 {
      *  \param type wanted type, default set to type but can
      *  be set to DbValue::Variant if waned
      */
-    DbValue(int val, Type type = Type::Int);
+    DbValue (int val, Type type = Type::Int);
 
     /**
      * \copydoc DbValue(int val, Type type = Type::Int)
      */
-    DbValue(int64_t val, Type type = Type::Int);
+    DbValue (int64_t val, Type type = Type::Int);
 
     /**
      * \copydoc DbValue(int val, Type type = Type::Int)
      */
-    DbValue(std::string val, Type type = Type::Text);
+    DbValue (std::string val, Type type = Type::Text);
 
     /**
      * \copydoc DbValue(int val, Type type = Type::Int)
      */
-    DbValue(double val, Type type = Type::Real);
+    DbValue (double val, Type type = Type::Real);
 
     /**
      * \copydoc DbValue(int val, Type type = Type::Int)
      */
-    DbValue(Blob val, Type type = Type::Blob) ;
-
-
+    DbValue (Blob val, Type type = Type::Blob);
 
     /**
      * \brief Destructor
      */
-    ~DbValue() noexcept;
+    ~DbValue () noexcept;
 
     /**
      * \brief Copy constructor
      */
-    DbValue(const DbValue&) noexcept;
+    DbValue (const DbValue&) noexcept;
 
     /**
      * \brief Move constructor
      */
-    DbValue(DbValue&&) noexcept;
-
-
+    DbValue (DbValue&&) noexcept;
 
     /** \brief Assignment
      *  \throw sl3::ErrTypeMisMatch if getType is incompatible
@@ -113,39 +103,37 @@ namespace sl3 {
      * \param val new value
      * \return reference to this
      */
-    DbValue& operator=(const DbValue& val);
+    DbValue& operator= (const DbValue& val);
 
     /**
      * \copydoc operator=(const DbValue& val)
      */
-    DbValue& operator=(DbValue&& val) ;
+    DbValue& operator= (DbValue&& val);
 
     /**
      * \copydoc operator=(const DbValue& val)
      */
-    DbValue& operator=(int val);
+    DbValue& operator= (int val);
 
     /**
      * \copydoc operator=(const DbValue& val)
      */
-    DbValue& operator=(const int64_t& val);
+    DbValue& operator= (const int64_t& val);
 
     /**
      * \copydoc operator=(const DbValue& val)
      */
-    DbValue& operator=(const double& val);
+    DbValue& operator= (const double& val);
 
     /**
      * \copydoc operator=(const DbValue& val)
      */
-    DbValue& operator=(const std::string& val);
+    DbValue& operator= (const std::string& val);
 
     /**
      * \copydoc operator=(const DbValue& val)
      */
-    DbValue& operator=(const Blob& val);
-
-
+    DbValue& operator= (const Blob& val);
 
     /** \brief Assignment
      *  \throw sl3::ErrTypeMisMatch if getType is incompatible
@@ -154,51 +142,49 @@ namespace sl3 {
      *  is a variant.
      *  \param val new value
      */
-    void set(int val);
+    void set (int val);
 
     /**
      * \copydoc set(int val)
      */
-    void set(int64_t val);
+    void set (int64_t val);
 
     /**
      * \copydoc set(int val)
      */
-    void set(const std::string& val);
+    void set (const std::string& val);
 
     /**
      * \copydoc set(int val)
      */
-    void set(double val);
+    void set (double val);
 
     /**
      * \copydoc set(int val)
      */
-    void set(const Blob& val) ;
-
+    void set (const Blob& val);
 
     /** \brief Value access
      *  \throw sl3::ErrNullValueAccess if value is null.
      *  \throw sl3::ErrTypeMisMatch if getType is incorrect
      *  \return reference to the value
      */
-    const int64_t& getInt() const;
+    const int64_t& getInt () const;
 
     /**
      * \copydoc getInt() const
      */
-    const double& getReal() const;
+    const double& getReal () const;
 
     /**
      * \copydoc getInt() const
      */
-    const std::string& getText() const;
+    const std::string& getText () const;
 
     /**
      * \copydoc getInt() const
      */
-    const Blob& getBlob() const;
-
+    const Blob& getBlob () const;
 
     /** \brief Value access with default for a NULL value.
      *
@@ -206,23 +192,22 @@ namespace sl3 {
      *  \param defval default value to return if value is NULL/
      *  \return the value or the give defval in case value is NULL
      */
-    int64_t getInt(int64_t defval) const;
+    int64_t getInt (int64_t defval) const;
 
     /**
      * \copydoc getInt(int64_t defval) const;
      */
-    double getReal(double defval) const;
+    double getReal (double defval) const;
 
     /**
      * \copydoc getInt(int64_t defval) const;
      */
-    std::string getText(const std::string& defval) const;
+    std::string getText (const std::string& defval) const;
 
     /**
      * \copydoc getInt(int64_t defval) const;
      */
-    Blob getBlob(const Blob& defval) const;
-
+    Blob getBlob (const Blob& defval) const;
 
     /** \brief Value access with default for a NULL and different type value.
      *
@@ -233,84 +218,80 @@ namespace sl3 {
      *  \return the value or the give default value if the value needs to be
      *      replaced
      */
-    int64_t get(int64_t defval) const;
+    int64_t get (int64_t defval) const;
 
     /**
      * \copydoc get(int64_t defval) const;
      */
-    int64_t get(int defval) const ;
+    int64_t get (int defval) const;
 
     /**
      * \copydoc get(int64_t defval) const;
      */
-    double get(double defval) const;
+    double get (double defval) const;
 
     /**
      * \copydoc get(int64_t defval) const;
      */
-    std::string get(const std::string& defval) const;
+    std::string get (const std::string& defval) const;
 
     /**
      * \copydoc get(int64_t defval) const;
      */
-    Blob get(const Blob& defval) const;
-
+    Blob get (const Blob& defval) const;
 
     /**
      * \brief Compare value for equality
      * \param val value to compare with
      * \return if given values is equal
      */
-    bool operator==(const int val) const  ;
+    bool operator== (const int val) const;
 
     /**
      * \copydoc operator==(const int val) const
      */
-    bool operator==(const int64_t& val) const  ;
+    bool operator== (const int64_t& val) const;
 
     /**
      * \copydoc operator==(const int val) const
      */
-    bool operator==(const std::string& val) const  ;
+    bool operator== (const std::string& val) const;
 
     /**
      * \copydoc operator==(const int val) const
      */
-    bool operator==(const double& val) const  ;
+    bool operator== (const double& val) const;
 
     /**
      * \copydoc operator==(const int val) const
      */
-    bool operator==(const Blob& val) const  ;
-
+    bool operator== (const Blob& val) const;
 
     /** \brief Compare value inequality.
      * \param val value to compare with
      * \return if given values are not equal
      */
-    bool operator!=(const int val) const ;
+    bool operator!= (const int val) const;
 
     /**
      * \copydoc operator!=(const int val) const
      */
-    bool operator!=(const int64_t& val) const ;
+    bool operator!= (const int64_t& val) const;
 
     /**
      * \copydoc operator!=(const int val) const
      */
-    bool operator!=(const std::string& val) const ;
+    bool operator!= (const std::string& val) const;
 
     /**
      * \copydoc operator!=(const int val) const
      */
-    bool operator!=(const double& val) const  ;
+    bool operator!= (const double& val) const;
 
     /**
      * \copydoc operator!=(const int val) const
      */
-    bool operator!=(const Blob& val) const  ;
-
-
+    bool operator!= (const Blob& val) const;
 
     /** \brief Moves the current value into the return value
      *
@@ -319,27 +300,23 @@ namespace sl3 {
      *  \throw sl3::ErrTypeMisMatch in case of wrong type.
      *  \return The value
      */
-    std::string ejectText();
-
+    std::string ejectText ();
 
     /**
      * \copydoc ejectText()
      */
-    Blob        ejectBlob();
-
+    Blob ejectBlob ();
 
     /**
      * \brief Set to NULL
      */
-    void setNull() ;
-
+    void setNull ();
 
     /**
      * \brief Check Null
      * \return if the value is null
      */
-    bool isNull() const ;
-
+    bool isNull () const;
 
     /**
      * \brief The Type of the value.
@@ -352,7 +329,7 @@ namespace sl3 {
      *
      * \return the type
      */
-    Type getType() const ;
+    Type getType () const;
 
     /**
      * \brief Returns the underlying type
@@ -363,7 +340,7 @@ namespace sl3 {
      *
      * \return the type the value actual holds
      */
-    Type getStorageType() const ;
+    Type getStorageType () const;
 
     /**
      * \brief Check if assignment would be OK
@@ -377,31 +354,29 @@ namespace sl3 {
     bool canAssign (const DbValue& other) const;
 
   private:
-
     const Type _type;
     // type never changes, even == makes just the value, not the type,
 
     Type _storageType;
     // that's the type for the union and what is applied in the db
-    union Store{
-        Store(){}
-        ~Store(){}
+    union Store
+    {
+      Store () {}
+      ~Store () {}
       int64_t     intval;
       double      realval;
       std::string textval;
       Blob        blobval;
-    } ;
+    };
 
     Store _store;
 
-    friend class DbValues ;
+    friend class DbValues;
 
     void assign (const DbValue& other);
 
-    void clearValue();
-
+    void clearValue ();
   };
-
 
   /**
    * \brief Steram op for a DbValue
@@ -410,60 +385,53 @@ namespace sl3 {
    * \param v the value to stream
    * \return ostream
    */
-  std::ostream&
-  operator<<(std::ostream& stm, const sl3::DbValue& v);
+  std::ostream& operator<< (std::ostream& stm, const sl3::DbValue& v);
 
+  // variant like access
 
-  // variant like access 
-
-
-  template< typename T >
-  struct always_false
-  { 
-    enum { value = false };  
+  template <typename T> struct always_false
+  {
+    enum
+    {
+      value = false
+    };
   };
-  
-  template<typename T>
-  inline
-  const T& get(const DbValue&)
+
+  template <typename T>
+  inline const T&
+  get (const DbValue&)
   {
-    static_assert ( always_false<T>::value,
-                    "Invalid type to get from DbValue!");
+    static_assert (always_false<T>::value,
+                   "Invalid type to get from DbValue!");
   }
 
-  template<>
-  inline
-  const int64_t&
-  get(const DbValue& v)
+  template <>
+  inline const int64_t&
+  get (const DbValue& v)
   {
-    return v.getInt () ;      
+    return v.getInt ();
   }
 
-  template<>
-  inline
-  const double&
-  get(const DbValue& v)
+  template <>
+  inline const double&
+  get (const DbValue& v)
   {
-    return v.getReal () ;      
-  }
-  
-  template<>
-  inline
-  const std::string&
-  get(const DbValue& v)
-  {
-    return v.getText () ;      
-  }
-  
-  template<>
-  inline
-  const Blob&
-  get(const DbValue& v)
-  {
-    return v.getBlob () ;      
+    return v.getReal ();
   }
 
-  
+  template <>
+  inline const std::string&
+  get (const DbValue& v)
+  {
+    return v.getText ();
+  }
+
+  template <>
+  inline const Blob&
+  get (const DbValue& v)
+  {
+    return v.getBlob ();
+  }
 }
 
 #endif /* DbValue_HPP_ */
