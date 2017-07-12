0332aa96fb7689ee8ed5654162237d5a073112e8
a4z <harald.achitz@gmail.com>
2017-07-12 08:51:31 +0200

    start implementing doctests

---
+++ tests/doctest/database/dbtest.cpp
@@ -0,0 +1,71 @@
+#include "../testing.hpp"
+#include <sl3/database.hpp>
+#include <sl3/error.hpp>
+
+#include <string>
+
+SCENARIO("creating a database")
+{
+  GIVEN ("a valaid and an invalid database name/path")
+  {
+    auto good = ":memory:" ;
+    auto bad = "/this/does/not/exist/123" ;
+
+    WHEN ("using the valid name/path")
+    {
+      THEN ("constructing the db works")
+      {
+        CHECK_NOTHROW(sl3::Database{good});
+      }
+    }
+
+    WHEN ("using the invalid name/path")
+    {
+      THEN ("constructing the db works")
+      {
+        CHECK_THROWS_AS(sl3::Database{bad}, sl3::SQLite3Error) ;
+      }
+    }
+  }
+}
+
+
+
+SCENARIO("creating some test data")
+{
+  GIVEN ("an im memory database")
+  {
+    sl3::Database db{":memory:"};
+
+    WHEN ("executing invalid SQL")
+    {
+      auto wrong_sql = "CRATE NABLE foo (f1, f2);" ;
+      THEN ("ecexute triggers an exception")
+      {
+        CHECK_THROWS_AS (db.execute (wrong_sql), sl3::SQLite3Error) ;
+      }
+
+    }
+
+    WHEN ("creating a table and some test data")
+    {
+      db.execute (
+          "CREATE TABLE tbltest (intFld INTEGER,txtFld TEXT, dblFld real );"
+          "INSERT INTO tbltest VALUES (1, 'eins', 1.111) ;"
+          "INSERT INTO tbltest VALUES (2, 'zwei', 2.22) ;"
+          "INSERT INTO tbltest VALUES (3, NULL, NULL) ;"
+      );
+
+      THEN ("I can veryfy that the table and some test data exists")
+      {
+        CHECK (db.getTotalChanges () == 3) ;
+        CHECK_NOTHROW (db.execute ("DELETE FROM tbltest;")) ;
+        CHECK (db.getTotalChanges () == 6) ;
+        CHECK (db.getRecentlyChanged () == 3) ;
+      }
+
+    }
+  }
+}
+
+
