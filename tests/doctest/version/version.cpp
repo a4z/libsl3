0efdc8695605c50231771ab0b2b64b6ebcebb9cf
a4z <harald.achitz@gmail.com>
2017-07-17 13:27:39 +0200

    add typname test, complete version test

--- tests/doctest/version/version.cpp
+++ tests/doctest/version/version.cpp
@@ -3,39 +3,38 @@
 
 #include <string>
 
-/// [doxygen_test]
 SCENARIO("check sqlite versions for library and app")
 {
   // this can be used as an example to add into your project
   // assure the libsl3 was compiled using the same header as you app
 
-  GIVEN ("the sqlite version libsl3 was comiled with")
+  GIVEN ("cmake genertated config")
   {
-    std::string libUsedVersion = sl3::sqliteCompiledVersion () ;
-
-    WHEN ("asking for runtime version")
+    WHEN ("comparing compile/runtime verion")
     {
-      std::string appUsedVersion = sl3::sqliteRuntimeVersion () ;
-
-      THEN ("these version need to be the same")
-      {
-        // TODO what to do if we use dynamice versions of this ?
-        CHECK (libUsedVersion == appUsedVersion) ;
+      THEN ("these version are the same")
+      { 
+        std::string compiledVersion = sl3::sqliteCompiledVersion() ;
+        CHECK(compiledVersion == sl3::sqliteRuntimeVersion());
       }
+    }
 
+    WHEN ("comparing the compile/runtime version numbers")
+    {
+      THEN ("they are the same")
+      {
+        CHECK_EQ ( sl3::sqliteCompiledVersionNumber(),
+                    sl3::sqliteRuntimeVersionNumber());
+      }
+    }
+
+    WHEN ("calculating the expected version")
+    {
+      THEN ("it is the compile version number")
+      {
+        auto expectedVersion =  3*1000000 + sl3::PATCH_VERSION ;
+        CHECK (expectedVersion == sl3::sqliteCompiledVersionNumber()) ;
+      }
     }
   }
 }
-/// [doxygen_test]
-//
-// TODO those are not ported
-//
-//    BOOST_CHECK(
-//        sqliteCompiledVersionNumber() == sqliteRuntimeVersionNumber()
-//    );
-//
-//    // check that the cmake file was updated correct
-//    auto expectedVersion =  3*1000000 + sl3::PATCH_VERSION ;
-//
-//    // check that the cmake file was updated correct
-//    BOOST_CHECK(expectedVersion == sqliteCompiledVersionNumber()) ;
