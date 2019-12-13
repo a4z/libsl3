bc03fbd3a01b78d29285ef12e283f9304ed72368
a4z <harald.achitz@gmail.com>
2019-12-13 22:41:02 +0100

    update CMake requirement from 2.8 to 3.0, adoption for policy CMP0054 since MSVC is also a variable

--- CMake/macros.cmake
+++ CMake/macros.cmake
@@ -30,7 +30,7 @@ if (NOT have_myMacros)
             set(perfixd "-D")
         elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "AppleClang")
             set(perfixd "-D")
-        elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
+        elseif(${MSVC})
             set(perfixd "/D")
         else()
 
