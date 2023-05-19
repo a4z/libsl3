e79252f5b7eb2acb22840253176557ad1e57df81
a4z <harald.achitz@gmail.com>
2023-05-19 23:16:21 +0200

    Doc related improveements

--- CMake/macros.cmake
+++ CMake/macros.cmake
@@ -1,50 +1,40 @@
 
+include_guard(GLOBAL)
 
-if (NOT have_myMacros)
-
-    # this is a debug function
-    macro(printvars)
-    set(list_var "${ARGN}")
-        foreach(arg IN LISTS list_var)
-            message(STATUS "*var ${arg}:   ${${arg}}")
-        endforeach()
-    endmacro(printvars)
-                
-    
-
-    MACRO(PREFIX_WITH pre what)
-        #message(STATUS ${what})
-        SET(tmp "")
-        FOREACH(element ${${what}})
-        	SET(tmp ${pre}${element} ${tmp})
-        ENDFOREACH(element)
-        SET(${what} ${tmp})
-    ENDMACRO(PREFIX_WITH)
-    
-    
-    MACRO(PREFIX_COMPILER_DEFINES deflist)
-
-        if(${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
-            set(perfixd "-D")    
-        elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
-            set(perfixd "-D")
-        elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "AppleClang")
-            set(perfixd "-D")
-        elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
-            set(perfixd "/D")
-        else()
-
-        endif()
-                
-        PREFIX_WITH(${perfixd} ${deflist})  
-        
-    ENDMACRO(PREFIX_COMPILER_DEFINES)
-
-
-    set( have_myMacros ON )
-
-endif (NOT have_myMacros)
+# this is a debug function
+macro(printvars)
+set(list_var "${ARGN}")
+    foreach(arg IN LISTS list_var)
+        message(STATUS "*var ${arg}:   ${${arg}}")
+    endforeach()
+endmacro(printvars)
 
 
 
+MACRO(PREFIX_WITH pre what)
+    #message(STATUS ${what})
+    SET(tmp "")
+    FOREACH(element ${${what}})
+        SET(tmp ${pre}${element} ${tmp})
+    ENDFOREACH(element)
+    SET(${what} ${tmp})
+ENDMACRO(PREFIX_WITH)
 
+
+MACRO(PREFIX_COMPILER_DEFINES deflist)
+
+    if(${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
+        set(perfixd "-D")
+    elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+        set(perfixd "-D")
+    elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "AppleClang")
+        set(perfixd "-D")
+    elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
+        set(perfixd "/D")
+    else()
+
+    endif()
+
+    PREFIX_WITH(${perfixd} ${deflist})
+
+ENDMACRO(PREFIX_COMPILER_DEFINES)
--- CMake/setup_doc.cmake
+++ CMake/setup_doc.cmake
@@ -1,36 +1,29 @@
+include_guard(GLOBAL)
 
+option(BUILD_DOCUMENTATION "make Doxygen documentation" ON)
+#das sollt da raus weil ja nur die option gefragt, die action jedoch includiert sein soll
+if(BUILD_DOCUMENTATION)
 
-if (NOT have_myDocSetup)
+    find_package( Doxygen )
 
-    option(BUILD_DOCUMENTATION "make Doxygen documentation" ON)
-#das sollt da raus weil ja nur die option gefragt, die action jedoch includiert sein soll    
-    if(BUILD_DOCUMENTATION)
-    
-        find_package( Doxygen )
-        
-        if(DOXYGEN_FOUND)
-        
-            if(DOT)
-                set(HAVE_DOT YES)
-            else(DOT)
-                set(HAVE_DOT NO)
-            endif(DOT)
-        
-            configure_file(${PROJECT_SOURCE_DIR}/doc/doxyfile.cmake
-                ${PROJECT_BINARY_DIR}/doc/${PROJECT_NAME}.doxyfile
-            )
-        
-            #todo, naming, would like to have doxydoc in all projects
-            #add_custom_target(doc${PROJECT_NAME} ${DOXYGEN}
-            add_custom_target(doc ${DOXYGEN}
-                ${PROJECT_BINARY_DIR}/doc/${PROJECT_NAME}.doxyfile
-            )
-    
-        endif(DOXYGEN_FOUND)
-        
-    endif(BUILD_DOCUMENTATION)
-    
-    
-    set(have_myDocSetup ON BOOL)    
+    if(DOXYGEN_FOUND)
 
-endif (NOT have_myDocSetup)
+        if(DOT)
+            set(HAVE_DOT YES)
+        else(DOT)
+            set(HAVE_DOT NO)
+        endif(DOT)
+
+        configure_file(${PROJECT_SOURCE_DIR}/doc/doxyfile.cmake
+            ${PROJECT_BINARY_DIR}/doc/${PROJECT_NAME}.doxyfile
+        )
+
+        #todo, naming, would like to have doxydoc in all projects
+        #add_custom_target(doc${PROJECT_NAME} ${DOXYGEN}
+        add_custom_target(doc ${DOXYGEN}
+            ${PROJECT_BINARY_DIR}/doc/${PROJECT_NAME}.doxyfile
+        )
+
+    endif(DOXYGEN_FOUND)
+
+endif(BUILD_DOCUMENTATION)
