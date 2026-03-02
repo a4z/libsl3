230cc7c56f194ce5e15ebc39034d331350ca7de7
a4z <harald.achitz@gmail.com>
2026-03-02 18:53:00 +0100

    Add new files

---
+++ tests/errors/errortest.cpp
@@ -0,0 +1,74 @@
+#include "../testing.hpp"
+#include <sl3/error.hpp>
+#include <sl3/container.hpp>
+#include <map>
+#include <vector>
+
+// these tests are mainly to raise coverage, and protect against accidental code removal
+
+SCENARIO ("checking error codes")
+{
+  using namespace sl3;
+  GIVEN ("the expected error codes")
+  {
+    std::map<sl3::ErrCode, std::string> expected = {
+        {ErrCode::SQL3Error, "SQLite3Error"},
+        {ErrCode::NoConnection, "NoConnection"},
+        {ErrCode::OutOfRange, "OutOfRange"},
+        {ErrCode::TypeMisMatch, "TypeMisMatch"},
+        {ErrCode::NullValueAccess , "NullValueAccess"},
+        {ErrCode::UNEXPECTED , "UNEXPECTED"},
+    };
+
+    WHEN ("checking the expected error code names")
+    {
+        THEN("all names are as expected"){
+
+            for(auto given: expected){
+                const std::string wanted = sl3::ErrCodeName(given.first);
+                CHECK (given.second == wanted);
+            }
+            
+
+        }
+    }
+  }
+
+  GIVEN ("an SQL3Error Type"){
+    
+    using Err = ErrType<ErrCode::SQL3Error>;
+
+    WHEN ("creating an ErrType with some random SqLite error value")
+    {
+        const Err e{1, "hello"};
+        THEN("the Error has the expected type")
+        {
+            CHECK(e.getId() == sl3::ErrCode::SQL3Error );
+        }
+    }
+  }
+
+}
+
+
+
+SCENARIO ("Container index errors throw sl3 errors")
+{
+    GIVEN ("A Container") 
+    {
+        using iContainer = sl3::Container <std::vector<int>>;
+        WHEN("access index out of range via at function")
+        {
+            iContainer ic { {1,2,3} };
+            const iContainer cic = ic;
+
+            THEN("exceptions are thrown")
+            {
+                CHECK_THROWS_AS(ic.at(3), sl3::ErrOutOfRange );
+                CHECK_THROWS_AS(cic.at(3),
+                 sl3::ErrOutOfRange );
+            }
+        }
+    }
+
+}
