e889ab76229bb9ecdf2aba48dcde674556aa4879
a4z <harald.achitz@gmail.com>
2026-05-25 15:49:16 +0200

    Prepare for bcr

--- cmake/project-setup.cmake
+++ cmake/project-setup.cmake
@@ -2,7 +2,7 @@ include_guard(GLOBAL)
 # Keep this here for now, but add a CI test that builds with 20 or 17
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
-set(CMAKE_CXX_STANDARD 23)
+set(CMAKE_CXX_STANDARD 17)
 
 set(CMAKE_C_STANDARD_REQUIRED ON)
 set(CMAKE_C_EXTENSIONS OFF)
